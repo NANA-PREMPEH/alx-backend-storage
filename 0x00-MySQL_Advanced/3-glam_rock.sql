44f3c072c61408727e9308feb440842610f037ae
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-04-30 21:56:01 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,6 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(split, 2021) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT  band_name,
+        IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%'
+ORDER BY 2 DESC; 
