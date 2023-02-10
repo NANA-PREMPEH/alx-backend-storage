b6ac00108a9c313cc80dd84eb9b74de5fa70722b
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-02-10 05:13:31 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
--- Ranks country of origins by the number of non-unique fans
---life span is diff between split and formed
-SELECT band_name,
-		TIMESTAMPDIFF(YEAR, `formed`, `split`) AS lifespan FROM metal_bands
+-- Lists all bands with Glam rock as their style,
+-- ranked by their longevity
+-- Column names must be: band_name & lifespan
+
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
