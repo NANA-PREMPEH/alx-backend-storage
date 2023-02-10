cb28428fe1e8bd3da0868ba16f5fddb9b993d01d
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-02-10 05:30:10 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Old school band lifespan by band and style
+-- List all bands with Glam rock
 SELECT band_name,
-	ifnull(split, YEAR(CURDATE())) - formed as lifespan
+	IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
