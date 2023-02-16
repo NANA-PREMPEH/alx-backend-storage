4957dac34bd98bdf1e7e988c5db62ef199ddaaf8
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-02-16 06:06:05 +0000

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
-""" Redis Module """
-
-from functools import wraps
+"""Implementing an expiring web cache and tracker"""
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
-redis_ = redis.Redis()
 
-
-def count_requests(method: Callable) -> Callable:
-    """ Decortator for counting """
+def access(method: Callable) -> Callable:
+    """decorator for get_page"""
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+    def count(url: str) -> str:
+        """track how many times a particular URL was accessed"""
+        redis_client = redis.Redis()
+        redis_client.incr(f'count:{url}')
+        cached = redis_client.get(f'cached:{url}')
+        if cached:
+            return cached.decode('utf-8')
+        res = method(url)
+        redis_client.setex(f'cached:{url}', 10, res)
+        return res
+    return count
 
-    return wrapper
 
-
-@count_requests
+@access
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    req = requests.get(url)
-    return req.text
+    """send request to url"""
+    return requests.get(url).text
+
+
+if __name__ == '__main__':
+    get_page('http://google.com')
